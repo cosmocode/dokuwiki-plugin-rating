90a6f51e3b71b991a54502679e93bef17cead7ec
E32k <Erik32konto@gmail.com>
2026-09-11 19:00:08 +0200

    Translation update (cs)

---
+++ lang/cs/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author E32k <Erik32konto@gmail.com>
+ */
+$lang['intro']                 = 'Byla tato stránka užitečná?';
